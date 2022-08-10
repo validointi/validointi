44d4d84abd95e8f1c74f2e06e94d7e75f171a6ca
Jeffrey Bosch <Jefiozie@users.noreply.github.com>
2022-08-10 06:46:18 +0000

    refactor: use inject func instead of constr. injection

--- projects/demo/src/app/sample-data.service.ts
+++ projects/demo/src/app/sample-data.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, inject} from '@angular/core';
 import { of } from 'rxjs';
 import { ValidationErrors, ValidatorRegistryService } from 'validator';
 import { create, only, enforce, warn, test } from 'vest';
@@ -23,7 +23,7 @@ const inMemoryDb = new Map<string, SampleData>();
 export class SampleDataService {
   getById = (id: string) => of(inMemoryDb.get(id));
   getAll = () => of(Array.from(inMemoryDb.values()));
-
+  vr = inject(ValidatorRegistryService);
   validate = this.vr.registerValidator('sample-data', validateSampleData);
 
   save = (data: SampleData) => {
@@ -32,9 +32,6 @@ export class SampleDataService {
     }
     inMemoryDb.set(data.id, data);
   }
-
-
-  constructor(private vr: ValidatorRegistryService) { }
 }
 
 const year = 365 * 24 * 60 * 60 * 1000
