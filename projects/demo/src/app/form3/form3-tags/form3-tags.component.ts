9f3e1b59318c021b292ea9519936661cf9e4e75a
Sander Elias <SanderElias@users.noreply.github.com>
2022-10-01 08:55:13 +0200

    refactor: smal improvments
    * add more sample,
    and invidial controll
    
    * update to latest packages
    
    * remove tests from demo
    
    * moar wi
    
    * clean up after fixing main
    
    * add missing packages to package.json
    
    * update eslint
    
    * Added some tests for utility functions
    
    * fix some edge-cases
    
    * fix typo and add another test
    
    * added more tests and make whitespace invalid as part of a path
    
    * work on validationstate
    
    * finally there!
    
    * fix broken test
    
    * better fix for breaking test
    
    * update peacock colors to be closer to logo
    
    * a more complex form sample
    
    * more complex sample form
    
    * WIP
    
    * final touches, and cleanup
    
    * cleanup
    
    * fix lag, and more cleanup

--- projects/demo/src/app/form3/form3-tags/form3-tags.component.ts
+++ projects/demo/src/app/form3/form3-tags/form3-tags.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject, Input, OnInit, ViewChild } from '@angular/core';
-import { FormsModule, NgForm, NgModel } from '@angular/forms';
+import { ControlContainer, FormsModule, NgForm, NgModel } from '@angular/forms';
 import { ValidationErrorHookUpDirective } from '../../form1/validationErrorHookUp.directive';
 
 @Component({
@@ -20,26 +20,9 @@ import { ValidationErrorHookUpDirective } from '../../form1/validationErrorHookU
     display: label;
   }
   `],
+  viewProviders: [{ provide: ControlContainer, useFactory: (form: NgForm) => form, deps: [NgForm] }],
 })
-export class Form3TagsComponent implements OnInit {
+export class Form3TagsComponent {
   @Input() tag?: string;
   @Input() index?: number;
-
-  /**
-   * those 2 are needed to register the control in the form
-   */
-  @ViewChild(NgModel, { static: true }) ngModel!: NgModel;
-  form = inject(NgForm);
-
-  constructor() { }
-
-  ngOnInit(): void {
-    /**
-     * this is a bit of a chore, but we need to add the control to the form manually,
-     * because when we are in a child component of a form, Angular doesn't do this for us somehow???
-     * that thus requires us to inject the form, and use viewchild to get ngModel, and then add the control to the form
-     * (When we have it, we can also se it to set the name of the control, which is used in the error messages)
-     */
-    this.form.addControl(this.ngModel);
-  }
 }
