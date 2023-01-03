1c465eb49c23ef213907252f51ee95e1a2d0c3a8
Jeffrey Bosch <Jefiozie@users.noreply.github.com>
2023-01-03 10:39:04 +0100

    feat: 🎸 initial setup for schematic support (#55)
    With this commit we add support to use ng add

---
+++ projects/validointi/core/schematics/ng-add/index.ts
@@ -0,0 +1,16 @@
+import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
+import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
+import { addPackageToPackageJson } from '../utils';
+
+/**
+ * Add @validointi/core to package.json
+ * @returns tree
+ */
+export function ngAdd(): Rule {
+  return (tree: Tree, context: SchematicContext) => {
+    addPackageToPackageJson(tree, '@validointi/core', '^1.0.0');
+    context.addTask(new NodePackageInstallTask());
+    context.logger.info(`@validointi/core is installed`);
+    return tree;
+  };
+}
